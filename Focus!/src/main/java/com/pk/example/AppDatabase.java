ae05cce4d1f09d0d984291c45cf90076ae2f1454
William <yuanx@usc.edu>
2017-10-13 14:35:10 -0700

    add prev and curr notification DAOs

--- Focus!/src/main/java/com/pk/example/AppDatabase.java
+++ Focus!/src/main/java/com/pk/example/AppDatabase.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
 
 import com.pk.example.dao.MinNotificationDao;
 import com.pk.example.dao.ProfileDao;
