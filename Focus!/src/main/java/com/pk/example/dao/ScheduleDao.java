2ec112764ae34ccb2c039e2fcecc137e3853f947
William <yuanx@usc.edu>
2017-10-13 13:50:05 -0700

    updating DAO files

--- Focus!/src/main/java/com/pk/example/dao/ScheduleDao.java
+++ Focus!/src/main/java/com/pk/example/dao/ScheduleDao.java
@@ -1,5 +1,29 @@
 package com.pk.example.dao;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
-public class ScheduleDao {
+import com.pk.example.Schedule;
+import com.pk.example.entity.ProfileEntity;
+import com.pk.example.entity.ScheduleEntity;
+
+import java.util.List;
+
+@Dao
+public interface ScheduleDao {
+    @Query("SELECT * FROM schedules")
+    LiveData<List<ScheduleEntity>> loadAllSchedules();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(List<ScheduleEntity> schedules);
+
+    @Query("select * from schedules where id = :scheduleID")
+    LiveData<ScheduleEntity> loadSchedule(int scheduleID);
+
+    @Query("select * from schedules where id = :scheduleID")
+    ScheduleEntity loadScheduleSync(int scheduleID);
+
 }
