eebfc0630a294d6412c811c587630342a6f1ac38
William <yuanx@usc.edu>
2017-10-13 14:08:39 -0700

    Singleton-ize database

--- Focus!/src/main/java/com/pk/example/AppDatabase.java
+++ Focus!/src/main/java/com/pk/example/AppDatabase.java
@@ -1,6 +1,7 @@
 package com.pk.example;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 
@@ -15,6 +16,20 @@ import com.pk.example.entity.ScheduleEntity;
 public abstract class AppDatabase extends RoomDatabase {
 
     static final String DATABASE_NAME = "db";
+
+    private static AppDatabase mInstance;
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (mInstance == null) {
+            mInstance =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "db")
+                            // allow queries on the main thread.
+                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+                            .allowMainThreadQueries()
+                            .build();
+        }
+        return mInstance;
+    }
 
     public abstract MinNotificationDao minNotificationDao();
 
