9e3b93d71ea251656b74825ffe3d5ceefea05be5
Rowen Wu <kathertw@usc.edu>
2017-10-14 17:58:47 -0700

    fixing working tree????

---
+++ Focus!/src/main/java/com/pk/example/AppDatabase.java
@@ -0,0 +1,40 @@
+package com.pk.example;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+import com.pk.example.dao.MinNotificationDao;
+import com.pk.example.dao.ProfileDao;
+import com.pk.example.dao.ScheduleDao;
+import com.pk.example.entity.MinNotificationEntity;
+import com.pk.example.entity.ProfileEntity;
+import com.pk.example.entity.ScheduleEntity;
+
+@Database(entities = {MinNotificationEntity.class, ProfileEntity.class, ScheduleEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    static final String DATABASE_NAME = "db";
+
+    private static AppDatabase mInstance;
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (mInstance == null) {
+            mInstance =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "db")
+                            // allow queries on the main thread.
+                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+                            .allowMainThreadQueries()
+                            .build();
+        }
+        return mInstance;
+    }
+
+    public abstract MinNotificationDao minNotificationDao();
+
+    public abstract ProfileDao profileDao();
+
+    public abstract ScheduleDao scheduleDao();
+}
