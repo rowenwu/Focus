691c5790a13ddfe0fbc5573a0644b161a5d14a3d
Rowen Wu <kathertw@usc.edu>
2017-10-15 21:28:51 -0700

    added scheduleviewmodel

--- Focus!/src/main/java/com/pk/example/SchedulesViewModel.java
+++ Focus!/src/main/java/com/pk/example/SchedulesViewModel.java
@@ -1,50 +1,53 @@
-//package com.pk.example;
-//
-//import android.app.Application;
-//import android.arch.lifecycle.AndroidViewModel;
-//import android.arch.lifecycle.LiveData;
-//
-//import com.pk.example.entity.ScheduleEntity;
-//
-//import java.util.List;
-//
-//public class SchedulesViewModel extends AndroidViewModel {
-//
-//    private final LiveData<List<ScheduleEntity>> schedules;
-//
-//    private AppDatabase appDatabase;
-//
-//    public SchedulesViewModel(Application application) {
-//        super(application);
-//
-//        appDatabase = AppDatabase.getDatabase(this.getApplication());
-//
-//        schedules = appDatabase.scheduleDao().loadAllSchedules();
-//    }
-//
-//
-//    public LiveData<List<BorrowModel>> getItemAndPersonList() {
-//        return itemAndPersonList;
-//    }
-//
-//    public void deleteItem(BorrowModel borrowModel) {
-//        new deleteAsyncTask(appDatabase).execute(borrowModel);
-//    }
-//
-//    private static class deleteAsyncTask extends AsyncTask<BorrowModel, Void, Void> {
-//
-//        private AppDatabase db;
-//
-//        deleteAsyncTask(AppDatabase appDatabase) {
-//            db = appDatabase;
-//        }
-//
-//        @Override
-//        protected Void doInBackground(final BorrowModel... params) {
-//            db.itemAndPersonModel().deleteBorrow(params[0]);
-//            return null;
-//        }
-//
-//    }
-//
-//}
+package com.pk.example;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.pk.example.dao.ScheduleDao;
+import com.pk.example.entity.ScheduleEntity;
+
+import java.util.List;
+
+public class SchedulesViewModel extends AndroidViewModel {
+
+    private final LiveData<List<ScheduleEntity>> schedules;
+
+    private AppDatabase appDatabase;
+
+    public SchedulesViewModel(Application application) {
+        super(application);
+
+        appDatabase = AppDatabase.getDatabase(this.getApplication());
+
+        schedules = appDatabase.scheduleDao().loadAllSchedules();
+    }
+
+
+    public LiveData<List<ScheduleEntity>> getSchedules() {
+        return schedules;
+    }
+
+    public void deleteItem(ScheduleEntity scheduleEntity) {
+        //execute not working???
+        new deleteAsyncTask(appDatabase).execute(scheduleEntity);
+    }
+
+    private static class deleteAsyncTask extends AsyncTask<ScheduleEntity, Void, Void> {
+
+        private AppDatabase db;
+
+        deleteAsyncTask(AppDatabase appDatabase) {
+            db = appDatabase;
+        }
+
+        @Override
+        protected Void doInBackground(final ScheduleEntity... params) {
+            db.scheduleDao().delete(params[0]);
+            return null;
+        }
+
+    }
+
+}
