1a4e611284d9497ddb2b0cd76529bb034f5a8a7d
Rowen Wu <kathertw@usc.edu>
2017-10-17 00:00:33 -0700

    toast works

--- Focus!/src/main/java/com/pk/example/ScheduleListActivity.java
+++ Focus!/src/main/java/com/pk/example/ScheduleListActivity.java
@@ -1,15 +1,5 @@
 package com.pk.example;
-//
-//
-//import java.util.List;
-//import android.app.ListActivity;
-//import android.app.ProgressDialog;
-//import android.content.pm.ApplicationInfo;
-//import android.content.pm.PackageManager;
-//import android.os.AsyncTask;
-//import android.os.Bundle;
-//package com.pk.example;
-//
+
 import java.util.ArrayList;
 import java.util.List;
 import android.app.AlertDialog;
@@ -65,6 +55,12 @@ public class ScheduleListActivity extends ListActivity {
         new LoadApplications().execute();
     }
 
+    public void buttonClicked(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                scheduleEntityList.size() + scheduleEntityList.get(0).getName(), Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
 //        super.onListItemClick(l, v, position, id);
@@ -119,8 +115,10 @@ public class ScheduleListActivity extends ListActivity {
                 database.scheduleDao().insert(fakeSchedule);
                 scheduleEntityList = database.scheduleDao().loadAllSchedulesSync();
             }
+
             listadaptor = new ScheduleAdapter(ScheduleListActivity.this,
                     R.layout.snippet_list_row, scheduleEntityList);
+
 
             return null;
         }
@@ -151,95 +149,5 @@ public class ScheduleListActivity extends ListActivity {
     }
 }
 
-
-
-//
-//
-//public class ScheduleListActivity extends ListActivity {
-//    //private DatabaseHelper dbHelper = null;
-//    //private SQLiteDatabase db = null;
-////    private List<ScheduleEntity> scheduleEntityList = null;
-//    private List<ApplicationInfo> applist = null;
-//    private AppAdapter listadaptor = null;
-//    private PackageManager packageManager = null;
-//
-//
-//
-//    //    private ScheduleAdapter listadaptor = null;
-//    private ScheduleDao scheduleDao;
-//    private AppDatabase database;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_schedule_list);
-//        database = AppDatabase.getDatabase(getApplicationContext());
-//
-//        // cleanup for testing some initial data
-////        database.scheduleDao().removeAllUsers();
-//        // add some data
-//
-////        getActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//        // Create new helper
-//        // Get the database. If it does not exist, this is where it will also be created.
-//
-//
-//
-//        new LoadSchedules().execute();
-//        ListView listView = getListView();
-//        //listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-//    }
-//
-//
-//
-//    private class LoadSchedules extends AsyncTask<Void, Void, Void> {
-//        private ProgressDialog progress = null;
-//
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//            //get the list of notifications
-//            //notificationList = db.getNotificationList();
-////            scheduleEntityList = database.scheduleDao().loadAllSchedulesSync();
-////            if (scheduleEntityList.size()==0) {
-////                //create a fake schedule to insert
-////                //if no schedules in db
-////                ScheduleEntity fakeSchedule = new ScheduleEntity(DummyDb.makeFakeSchedule("There are no schedules to display.", 5));
-////                scheduleDao.insert(fakeSchedule);
-////                scheduleEntityList = database.scheduleDao().loadAllSchedulesSync();
-////            }
-//            applist = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
-//            listadaptor = new AppAdapter(ScheduleListActivity.this,
-//                    R.layout.snippet_list_row, applist);
-////            listadaptor = new ScheduleAdapter(ScheduleListActivity.this,
-////                    R.layout.schedule_list_row, scheduleEntityList);
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onCancelled() {
-//            super.onCancelled();
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void result) {
-//            setListAdapter(listadaptor);
-//            progress.dismiss();
-//            super.onPostExecute(result);
-//        }
-//
-//        @Override
-//        protected void onPreExecute() {
-//            progress = ProgressDialog.show(ScheduleListActivity.this, null,
-//                    "Loading notifications...");
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected void onProgressUpdate(Void... values) {
-//            super.onProgressUpdate(values);
-//        }
-//    }
-//}
 
 
