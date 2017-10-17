89b58a3ba254d0cb3f95e89f4ccb960a066245a8
Rowen Wu <kathertw@usc.edu>
2017-10-16 23:43:31 -0700

    edited schedulelistactivity

--- Focus!/src/main/java/com/pk/example/MainActivity.java
+++ Focus!/src/main/java/com/pk/example/MainActivity.java
@@ -95,6 +95,11 @@ public class MainActivity extends Activity {
         else if(v.getId() == R.id.btnPermission){
             toggleService(v);
         }
+        else if(v.getId() == R.id.btnCreateSchedule){
+//            Intent i = new Intent(this, ScheduleViewActivity.class);
+//            i.putExtra("flag", "create");
+//            startActivity(i);
+        }
         else if(v.getId() == R.id.btnCreateProfile){
             Intent i = new Intent(this, ProfileViewActivity.class);
             i.putExtra("flag", "create");
