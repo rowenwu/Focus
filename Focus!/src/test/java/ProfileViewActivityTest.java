4e71a43ffaee8e2f81e3ea4673231f584ea74b12
Stephanie Lee <lee970@usc.edu>
2017-10-30 00:50:57 -0700

    junit test backup ver

---
+++ Focus!/src/test/java/ProfileViewActivityTest.java
@@ -0,0 +1,16 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.pk.example.ProfileViewActivity;
+
+/**
+ * Created by Stephanie on 10/29/2017.
+ */
+
+public class ProfileViewActivityTest {
+    @Test
+    public void test() {
+        //
+    }
+}
