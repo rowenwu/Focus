f75bf57f4a5bc0e8a3b35a9366b73d794e2c4eda
Andy Ly <andyly@usc.edu>
2017-10-30 23:11:38 -0700

    Update CreateProfileTest.java

--- Focus!/src/androidTest/java/CreateProfileTest.java
+++ Focus!/src/androidTest/java/CreateProfileTest.java
@@ -7,6 +7,7 @@ import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
@@ -55,6 +56,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 /**
@@ -76,6 +78,15 @@ public class CreateProfileTest {
     @Rule
     public ActivityTestRule<MainActivity> homeActivityRule =
             new ActivityTestRule<>(MainActivity.class, true, false);
+
+    @Test
+    public void aCheckNoProfileToDisplayTest() {
+        //go to schedule list
+        onView(ViewMatchers.withId(R.id.btnAllProfiles)).perform(click());
+
+        //check no profiles
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("There are no profiles to display."))))));
+    }
 
     @Test
     public void createAProfileAndAddToProfileListWithOneAppTest() {
@@ -107,218 +118,159 @@ public class CreateProfileTest {
         onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(0).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("Focus!"))))));
     }
 
-//   // @Test
-//    public void createAProfileAndAddToProfileListWithMultipleAppTest() {
-//        //go to profilelist
-//        onView(withId(R.id.btnAllProfiles)).perform(click());
-//
-//        //click create profile
-//        onView(withId(R.id.btnToast)).perform(click());
-//
-//        //check if goes to create profile view
-//        onView(withId(R.id.textView)).check(ViewAssertions.matches((withText("Profile Name"))));
-//
-//        //type in profile name
-//        onView(withId(R.id.editTextProfileName)).perform(typeText("MultipleProfile"), closeSoftKeyboard());
-//
-//        //choose apps to block
-//        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(0).perform(click());
-//        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(1).perform(click());
-//        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(2).perform(click());
-//
-//        //creates the profile
-//        onView(withId(R.id.btnCreateProfile)).perform(click());
-//
-//        //check if profile in list
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(1).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("MultipleProfile"))))));
-//
-//        //click the profile to check if app is listed
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(1).perform(click());
-//
-//        //check if apps are listed
-//        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(0).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("com.android.cts.priv.ctsshim"))))));
-//        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(1).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("YouTube"))))));
-//        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(2).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("Android Services Library"))))));
-//    }
-//
-//
-//    @Test
-//    public void createScheduleAndAddToScheduleListWithOneProfileTest() {
-//        //go to schedule list
-//        onView(withId(R.id.btnNavigation)).perform(click());
-//
-//        //click create schedule button
-//        onView(withId(R.id.btnToast)).perform(click());
-//
-//        //type in schedule name
-//        onView(withId(R.id.editTextProfileName)).perform(typeText("SingleSchedule"), closeSoftKeyboard());
-//
-//        //select date
-//        onView(withId(R.id.btn_date)).perform(click());
-//
-//        //set date
-//        onView(withClassName((Matchers.equalTo(DatePicker.class.getName())))).perform(PickerActions.setDate(2018, 10, 21));
-//
-//        //confirm date
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        //select time
-//        onView(withId(R.id.btn_time)).perform(click());
-//
-//        //set time
-//        onView(withClassName((Matchers.equalTo(TimePicker.class.getName())))).perform(PickerActions.setTime(10, 0));
-//
-//        //confirm time
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        //select duration
-//        onView(withId(R.id.btn_duration)).perform(click());
-//
-//        //set duration
-//        onView(withClassName((Matchers.equalTo(TimePicker.class.getName())))).perform(PickerActions.setTime(1, 0));
-//
-//        //confirm duration
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        //add profile
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).perform(click());
-//
-//        //click create button
-//        onView(withId(R.id.btnCreateProfile)).perform(click());
-//
-//        //check if schedule is in list
-//        onData(allOf(instanceOf((ScheduleEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("SingleSchedule"))))));
+    @Test
+    public void createAProfileAndAddToProfileListWithMultipleAppTest() {
+        //go to profilelist
+        onView(withId(R.id.btnAllProfiles)).perform(click());
 
-//        //click the profile to check if app is listed
-//        onData(allOf(instanceOf((ScheduleEntity.class)))).atPosition(0).perform(click());
-//
-//        //check if app is listed
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("SingleProfile"))))));
-//    }
+        //click create profile
+        onView(withId(R.id.btnToast)).perform(click());
 
-//    @Test
-//    public void createScheduleAndAddToScheduleListWithMultipleProfileTest() {
-//        //go to schedule list
-//        onView(withId(R.id.btnNavigation)).perform(click());
-//
-//        //click create schedule button
-//        onView(withId(R.id.btnToast)).perform(click());
-//
-//        //type in schedule name
-//        onView(withId(R.id.editTextProfileName)).perform(typeText("SingleSchedule"), closeSoftKeyboard());
-//
-//        //select date
-//        onView(withId(R.id.btn_date)).perform(click());
-//
-//        //set date
-//        onView(withClassName((Matchers.equalTo(DatePicker.class.getName())))).perform(PickerActions.setDate(2018, 10, 21));
-//
-//        //confirm date
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        //select time
-//        onView(withId(R.id.btn_time)).perform(click());
-//
-//        //set time
-//        onView(withClassName((Matchers.equalTo(TimePicker.class.getName())))).perform(PickerActions.setTime(10, 0));
-//
-//        //confirm time
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        //select duration
-//        onView(withId(R.id.btn_duration)).perform(click());
-//
-//        //set duration
-//        onView(withClassName((Matchers.equalTo(TimePicker.class.getName())))).perform(PickerActions.setTime(1, 0));
-//
-//        //confirm duration
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        //add profiles
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).perform(click());
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(1).perform(click());
-//
-//        //click create button
-//        onView(withId(R.id.btnCreateProfile)).perform(click());
-//
-//        //check if schedule is in list
-//        onData(allOf(instanceOf((ScheduleEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("SingleSchedule"))))));
-//
-//        //click the profile to check if app is listed
-//        onData(allOf(instanceOf((ScheduleEntity.class)))).atPosition(0).perform(click());
-//
-//        //check if app is listed
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("SingleProfile"))))));
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(1).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("MultipleProfile"))))));
-//
-//    }
+        //check if goes to create profile view
+        onView(withId(R.id.textView)).check(ViewAssertions.matches((withText("Profile Name"))));
+
+        //type in profile name
+        onView(withId(R.id.editTextProfileName)).perform(typeText("MultipleProfile"), closeSoftKeyboard());
+
+        //choose apps to block
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(0).perform(click());
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(1).perform(click());
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(2).perform(click());
+
+        //creates the profile
+        onView(withId(R.id.btnCreateProfile)).perform(click());
+
+        //check if profile in list
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches((withText(containsString(("MultipleProfile"))))));
+
+        //click the profile to check if app is listed
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).perform(click());
+
+        //check if apps are listed
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(0).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("com.android.cts.priv.ctsshim"))))));
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(1).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("YouTube"))))));
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(2).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("Android Services Library"))))));
+    }
 
 
-//    @Test
-//    public void turnProfileActiveAndBlockingNotificationTest() {
-//        //Espresso Code
-//        //go to profilelist
-//        onView(withId(R.id.btnAllProfiles)).perform(click());
-//
-//        //toggle on profile to be active
-//        //click the profile to check if app is listed
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.toggBtn)).perform(click());
-//
-//        //go back to main activity
-//        //pressBack();
-//        //onView(withContentDescription("Navigate up")).perform(click());
-//        //onView(isRoot()).perform(ViewActions.pressMenuKey());
-////        onView(withParent((withClassName(is(Toolbar.class.getName()))))).perform(click());
-//        //DrawerActions.openDrawer((R.id.my_drawer_layout));
-//        //pressBack();
-//        //Intent intent = new Intent();
-//        //homeActivityRule.launchActivity(intent);
-//
-//
-//
-//        //UIAutomator Code
-//        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-//        device.openNotification();
-//        device.wait(Until.hasObject(By.text("Focus!")), 100000000);
-//        UiObject2 title = device.findObject(By.text("Focus!"));
-//        Assert.assertEquals("Focus!", title.getText());
-//
-//
-////        //click on send notification
-////        onView(withId(R.id.btnCreateNotify)).perform(click());
-////
-////        //UIAutomator Code
-////        //check if notification was blocked
-////        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-////        device.openNotification();
-////        device.wait(Until.hasObject(By.text("PK Notification")), 10000);
-////        UiObject2 title = device.findObject(By.text("PK Notification"));
-////        Assert.assertNull(title);
-//        //Assert.assertEquals("Notification Listener Example", text.getText());
-//
-//    }
+    @Test
+    public void turnProfileActiveAndBlockingNotificationTest() {
+        //go to profilelist
+        onView(withId(R.id.btnAllProfiles)).perform(click());
 
-//    @Test
-//    public void deleteProfileAndCheckIfInProfileList() {
-//        //go to profilelist
-//        onView(withId(R.id.btnAllProfiles)).perform(click());
-//
-//        //click the profile to delete
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).perform(click());
-//
-//        //click to edit mode
-//        onView(withId(R.id.btnEditProfile)).perform(click());
-//
-//        //click delete profile
-//        onView(withId(R.id.btnDeleteProfile)).perform(click());
-//
-//        //click the profile to delete
-//        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).perform(click());
-//
-//
+        //click create profile
+        onView(withId(R.id.btnToast)).perform(click());
 
-    //
-//    }
+        //check if goes to create profile view
+        onView(withId(R.id.textView)).check(ViewAssertions.matches((withText("Profile Name"))));
+
+        //type in profile name
+        onView(withId(R.id.editTextProfileName)).perform(typeText("ActiveTestProfile"), closeSoftKeyboard());
+
+        //choose app to block 44
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(44).perform(click());
+
+        //creates the profile
+        onView(withId(R.id.btnCreateProfile)).perform(click());
+
+        //Espresso Code
+        //toggle on profile to be active
+        //click the profile to check if app is listed
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.toggBtn)).perform(click());
+
+        //go back to main activity
+
+        Intent intent = new Intent();
+        homeActivityRule.launchActivity(intent);
+
+        //click on send notification
+        onView(withId(R.id.btnCreateNotify)).perform(click());
+
+        //UIAutomator Code
+        //check if notification was blocked
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        device.openNotification();
+        device.wait(Until.hasObject(By.text("PK Notification")), 10000);
+        UiObject2 title = device.findObject(By.text("PK Notification"));
+        Assert.assertNull(title);
+    }
+
+    @Test
+    public void deleteProfileAndCheckIfInProfileList() {
+        //go to profilelist
+        onView(withId(R.id.btnAllProfiles)).perform(click());
+
+        //click create profile
+        onView(withId(R.id.btnToast)).perform(click());
+
+        //check if goes to create profile view
+        onView(withId(R.id.textView)).check(ViewAssertions.matches((withText("Profile Name"))));
+
+        //type in profile name
+        onView(withId(R.id.editTextProfileName)).perform(typeText("DeleteTestProfile"), closeSoftKeyboard());
+
+        //choose app to block 44
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(44).perform(click());
+
+        //creates the profile
+        onView(withId(R.id.btnCreateProfile)).perform(click());
+
+        //click the profile to delete
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).perform(click());
+
+        //click to edit mode
+        onView(withId(R.id.btnEditProfile)).perform(click());
+
+        //close keyboard
+        onView(withId(R.id.editTextProfileName)).perform(closeSoftKeyboard());
+
+        //click delete profile
+        onView(withId(R.id.btnDeleteProfile)).perform(click());
+
+        //check is profile still in list
+        onData(allOf(instanceOf((ScheduleEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).check(ViewAssertions.matches(not((withText(containsString(("testDeleteProfile"))))));
+
+    }
+
+    @Test
+    public void editProfileAppsTest() {
+        //go to profilelist
+        onView(withId(R.id.btnAllProfiles)).perform(click());
+
+        //click create profile
+        onView(withId(R.id.btnToast)).perform(click());
+
+        //type in profile name
+        onView(withId(R.id.editTextProfileName)).perform(typeText("ActiveTestProfile"), closeSoftKeyboard());
+
+        //choose app to block 44
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(44).perform(click());
+
+        //creates the profile
+        onView(withId(R.id.btnCreateProfile)).perform(click());
+
+        //click profile to be edited
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).perform(click());
+
+        //click edit button
+        onView(withId(R.id.btnEditProfile)).perform(click());
+
+        //close keyboard
+        onView(withId(R.id.editTextProfileName)).perform(closeSoftKeyboard());
+
+        //choose app to block 3
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(3).perform(click());
+
+        //new applicatin slected
+        onView(withId(R.id.btnSaveProfile)).perform(click());
+
+        //click profile to be edited
+        onData(allOf(instanceOf((ProfileEntity.class)))).atPosition(0).onChildView(withId(R.id.name)).perform(click());
+
+        //check if new app
+        onData(allOf(instanceOf((ApplicationInfo.class)))).atPosition(0).onChildView(withId(R.id.app_name)).check(ViewAssertions.matches((withText(containsString(("Example Wallpapers"))))));
+
+    }
 
 
 
